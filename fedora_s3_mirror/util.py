66e9e4a58cd1fcae0c1bfc2d7f03da257008ccd6
William Coe <willcoe@aiven.io>
2020-04-22 12:20:40 +1000

    Assert checksums match instead of custom exception

--- fedora_s3_mirror/util.py
+++ fedora_s3_mirror/util.py
@@ -7,17 +7,12 @@ from requests.adapters import HTTPAdapter
 from urllib3 import Retry
 
 
-class InvalidChecksumError(Exception):
-    pass
-
-
 def validate_checksum(path, checksum_type, checksum) -> None:
     if checksum_type != "sha256":
         raise ValueError("Only sha256 checksums are currently supported")
     with open(path, "rb") as f:
         local_checksum = hashlib.sha256(f.read()).hexdigest()
-        if checksum != local_checksum:
-            raise InvalidChecksumError(f"{path}: expected {checksum} found {local_checksum}")
+        assert checksum == local_checksum, f"{path}: expected {checksum} found {local_checksum}"
 
 
 def get_requests_session() -> Session:
