9391206af498ddfe4c81f2aa23b2d772902425a0
Roman Inflianskas <rominf@aiven.io>
2023-01-26 16:40:05 +0200

    Switch to modern way of Python packaging
    Fix #27.

--- setup.py
+++ setup.py
@@ -1,36 +1,9 @@
 # Copyright (c) 2020 Aiven, Helsinki, Finland. https://aiven.io/
 
-from setuptools import setup
-import version
+# a setup.py shim which is required for an editable install
+# see pyproject.toml for build configuration
+# https://peps.python.org/pep-0518
+import setuptools
 
-version = version.get_project_version("rpm_s3_mirror/version.py")
-
-setup(
-    name="rpm_s3_mirror",
-    packages=["rpm_s3_mirror"],
-    version=version,
-    description="Tool for syncing RPM repositories with S3",
-    license="Apache 2.0",
-    author="Aiven",
-    author_email="willcoe@aiven.io",
-    url="https://github.com/aiven/rpm-s3-mirror",
-    install_requires=[
-        "defusedxml",
-        "requests",
-        "python-dateutil",
-        "botocore",
-        "lxml",
-    ],
-    entry_points={
-        "console_scripts": [
-            "rpm_s3_mirror = rpm_s3_mirror.__main__:main",
-        ],
-    },
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Intended Audience :: Information Technology",
-        "Intended Audience :: System Administrators",
-        "Programming Language :: Python :: 3.7",
-        "Natural Language :: English",
-    ],
-)
+if __name__ == "__main__":
+    setuptools.setup()
